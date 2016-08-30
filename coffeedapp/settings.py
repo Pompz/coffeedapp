2cf7cd2091740ae9d3d558cb0e6085cde1e6eba8
Pompz <paulo.pompilio@gmail.com>
2016-08-30 14:18:33 +0100

    Design

--- coffeedapp/settings.py
+++ coffeedapp/settings.py
@@ -134,7 +134,7 @@ ALLOWED_HOSTS = ['*']
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 AWS_S3_FORCE_HTTP_URL = True
-AWS_QUERYSTRING_AUTH = False
+AWS_QUERYSTRING_AUTH = True
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWSSecretKey')
 AWS_ACCESS_KEY_ID = os.environ.get('AWSAccessKeyId')
 
